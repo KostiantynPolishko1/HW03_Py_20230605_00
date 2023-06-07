2c2d6b228f83187693b72eb2aa416a9bc85b4979
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-07 08:42:23 +0300

    0 Start point

---
+++ main.py
@@ -0,0 +1,12 @@
+intOper = abs(int(input("\tOperation.\n\tEnter number: ")))
+
+if intOper == 1:
+    print("\n\tTask 1")
+    intA = float(input("\tEnter value A = "))
+    intB = float(input("\tEnter value B = "))
+
+if intOper == 2:
+    print("\n\tTask 2")
+
+if intOper == 3:
+    print("\n\tTask 3")
